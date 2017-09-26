07ef7abab1a2826b12878d08961d50ed2c6093cb
Matt Hough <matt@hough.tv>
2017-09-26 00:22:08 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,10 +1,13 @@
 def hello_t(array)
+  if block_given?
     i = 0
     while i < array.length
       yield array[i]
       i += 1
     end
     array
+  else
+    puts "Bruh. You gotta give a block."
 end
 
 # call your method here!
